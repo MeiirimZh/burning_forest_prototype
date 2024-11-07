9f47f287f924a18b52692c59dd79ef2a7d3f26dd
MeiirimZh <zh.meyirim@gmail.com>
2024-11-07 11:05:33 +0500

    Added "jump" (J key) in Input Map and added jump for player

--- scripts/spirit.gd
+++ scripts/spirit.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
 @export var speed := 7.0
+@export var jump_force := 10.0
+@export var gravity := 30.0
 
 func _physics_process(_delta) -> void:
 	var direction = 0
@@ -11,5 +13,11 @@ func _physics_process(_delta) -> void:
 		direction = 1
 		
 	velocity.x = direction * speed
+	
+	if not is_on_floor():
+		velocity.y -= gravity * _delta
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
 	
 	move_and_slide()
