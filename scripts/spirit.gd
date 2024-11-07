015a2a2076b40b1c3bf915f2e5c6b05294e24ed4
MeiirimZh <zh.meyirim@gmail.com>
2024-11-07 15:20:13 +0500

    Fixed a front jump animation

--- scripts/spirit.gd
+++ scripts/spirit.gd
@@ -11,6 +11,8 @@ extends CharacterBody3D
 
 # Variables
 @export var state := "r_idle"
+var is_jumping := false
+var last_direction = "r"
 
 # Rotate the mesh and play an animation 
 func rotate_and_play(angle, animation):
@@ -20,6 +22,7 @@ func rotate_and_play(angle, animation):
 func _physics_process(_delta) -> void:
 	var direction = 0
 	
+	# Check the state and play the corresponding animation
 	if state == "r_idle":
 		rotate_and_play(0, "idle")
 	elif state == "l_idle":
@@ -30,14 +33,20 @@ func _physics_process(_delta) -> void:
 		rotate_and_play(92.5, "run")
 	elif state == "front_jump":
 		animation_player.play("jump_front")
+	elif state == "front_fall":
+		animation_player.play("fall_front")
 	
+	# Running
 	if Input.is_action_pressed("left"):
 		direction = -1
 		state = "l_run"
+		last_direction = "l"
 	elif Input.is_action_pressed("right"):
 		direction = 1
 		state = "r_run"
-		
+		last_direction = "r"
+	
+	# Reset the state to idle after a movement
 	if Input.is_action_just_released("left"):
 		state = "l_idle"
 	elif Input.is_action_just_released("right"):
@@ -47,9 +56,19 @@ func _physics_process(_delta) -> void:
 	
 	if not is_on_floor():
 		velocity.y -= gravity * _delta
+		if velocity.y < 0 and is_jumping:
+			state = "front_fall"
+	else:
+		if is_jumping:
+			is_jumping = false
+			if last_direction == "r":
+				state = "r_idle"
+			else:
+				state = "l_idle"
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
+		is_jumping = true
 		if velocity.x == 0:
 			state = "front_jump"
 	
