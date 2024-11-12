979f883e07f19502616829d112894b9f8ce8794a
MeiirimZh <zh.meyirim@gmail.com>
2024-11-12 11:50:15 +0500

    Added check of player's position

--- scripts/merc.gd
+++ scripts/merc.gd
@@ -1,11 +1,25 @@
 extends CharacterBody3D
 
+# Nodes
+@onready var animation_player = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# Scenes
+@onready var player = get_node("/root/World/Spirit")
 
+# Variables
+var player_detected = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if player_detected:
+		if position.y in range(player.position.y-2, player.position.y+0.5):
+			animation_player.play("idle_straight")
+		else:
+			animation_player.play("idle_up")
+
+func _on_detect_player_body_entered(body: Node3D) -> void:
+	if body.name == "Spirit":
+		player_detected = true 
+
+func _on_detect_player_body_exited(body: Node3D) -> void:
+	if body.name == "Spirit":
+		player_detected = false
