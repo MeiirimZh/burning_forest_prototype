82df0519c02074062effd1075faedc19fcdcd3b4
MeiirimZh <zh.meyirim@gmail.com>
2024-11-13 11:04:46 +0500

    Added an up attack

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -1,16 +1,17 @@
 extends Area3D
 
 # Physics variables
-@export var h_speed := 8.0
-@export var v_speed := 0
+@export var speed := 8.0
 
 # Variables
 var direction := -1
+var type := 'straight'
 
 # Functions
-func move(delta, y=0):
-	position.x += direction * h_speed * delta
-	position.y += direction * y * delta
+func move(delta):
+	position.x += direction * speed * delta
+	if type == 'up':
+		position.y += speed * delta
 
 func _physics_process(delta: float) -> void:
-	move(delta, v_speed)
+	move(delta)
--- scripts/merc.gd
+++ scripts/merc.gd
@@ -16,18 +16,25 @@ var can_attack := true
 var direction := -1
 
 # Functions
-func spawn_projectile(delta, v_speed):
+func spawn_projectile():
 	var projectile_instance = projectile_scene.instantiate()
 	var spawn_position
-	if direction == 1:
+	
+	if abs(position.y - player.position.y) > 1.3:
+		projectile_instance.type = 'up'
+	
+	# Align the spawn position based on direction and attack type
+	if direction == 1 and projectile_instance.type == "straight":
+		spawn_position = self.global_position + Vector3(direction, 1.2, 0)
+	elif direction == -1 and projectile_instance.type == "straight":
+		spawn_position = self.global_position + Vector3(direction-0.4, 1.2, 0)
+	elif direction == 1 and projectile_instance.type == "up":
 		spawn_position = self.global_position + Vector3(direction, 1.2, 0)
 	else:
-		spawn_position = self.global_position + Vector3(direction-0.6, 1.2, 0)
+		spawn_position = self.global_position + Vector3(direction, 1.4, 0)
 	
 	projectile_instance.direction = direction
 	projectile_instance.position = spawn_position
-	
-	projectile_instance.move(delta, v_speed)
 	
 	get_tree().current_scene.add_child(projectile_instance)
 
@@ -36,7 +43,7 @@ func rotate_and_play(angle, animation):
 	mesh.rotation.y = angle
 	animation_player.play(animation)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if player_detected:
 		if abs(position.y - player.position.y) <= 1.3 and position.x >= player.position.x:
 			rotate_and_play(0, "idle_straight")
@@ -48,13 +55,9 @@ func _process(delta: float) -> void:
 			animation_player.play("idle_up")
 	
 	if player_detected and can_attack:
+		spawn_projectile()
 		can_attack = false
 		attack_cooldown_timer.start(attack_cooldown_duration)
-		if abs(position.y - player.position.y) > 1.3:
-			spawn_projectile(delta, 8.0)
-		else:
-			spawn_projectile(delta, 0)
-		
 
 func _on_detect_player_body_entered(body: Node3D) -> void:
 	if body.name == "Spirit":
