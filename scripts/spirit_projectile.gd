7cc9e19c42cc2de7b0254b3da93ece5de3911e7b
MeiirimZh <zh.meyirim@gmail.com>
2024-11-14 20:00:12 +0500

    Changed type of SpiritProjectile to Node3D

--- scripts/spirit_projectile.gd
+++ scripts/spirit_projectile.gd
@@ -1,7 +1,8 @@
-extends CharacterBody3D
+extends Node3D
 
 # Nodes
 @onready var dtimer = $DisappearanceTimer
+@onready var ray = $RayCast3D
 
 # Physics variables
 @export var speed := 8.0
@@ -15,6 +16,8 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	position.x += direction * speed * delta
+	if ray.is_colliding():
+		queue_free()
 
 func _on_disappearance_timer_timeout() -> void:
 	queue_free()
