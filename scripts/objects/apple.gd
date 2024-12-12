4d672aca5c33204bdd9ad13fe41f18e09b3e13d5
MeiirimZh <zh.meyirim@gmail.com>
2024-12-12 13:34:12 +0500

    Made abstraction for player's max hp in apple.gd

--- scripts/objects/apple.gd
+++ scripts/objects/apple.gd
@@ -1,11 +1,18 @@
 extends Area3D
 
+# Scenes
+@onready var player = get_node("/root/World/Spirit")
+
 # Variables
 @export var mode := "heal"
+var player_max_hp : int
+
+func _ready() -> void:
+	player_max_hp = player.hp
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "Spirit":
-		if body.hp < 5 and mode == "heal":
+		if body.hp < player_max_hp and mode == "heal":
 			body.hp += 1
 			Global.player_healed = true
 			
