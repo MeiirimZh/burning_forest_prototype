b2cb869d1a834be467db25b2f418dab112995a4e
MeiirimZh <zh.meyirim@gmail.com>
2024-12-16 17:55:26 +0500

    Flower changes depending on wherever character is good or not

--- scripts/gui/dialogue_menu.gd
+++ scripts/gui/dialogue_menu.gd
@@ -4,16 +4,22 @@ extends CanvasLayer
 @onready var speaker = $Control/MarginContainer/MarginContainer/HBoxContainer/VBoxContainer/Speaker
 @onready var speech = $Control/MarginContainer/MarginContainer/HBoxContainer/VBoxContainer/Speech
 @onready var portrait_texture_rect = $Control/Portrait
+@onready var flower = $Control/Flower
 
 # Portraits
 @onready var spirit_anxiety = preload("res://images/portraits/SpiritAnxiety.png")
 @onready var spirit_shock = preload("res://images/portraits/SpiritShock.png")
 var portraits
 
+# Flowers
+@onready var living_flower = preload("res://images/icons/Flower.png")
+@onready var dead_flower = preload("res://images/icons/DeadFlower.png")
+
 # Variables
 var current_key : String
 var in_progress := false
 var i := 0
+var good_characters = ["Spirit"]
 
 func _ready() -> void:
 	hide()
@@ -33,11 +39,18 @@ func start_dialogue(key):
 	show()
 	in_progress = true
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if in_progress:
+		# Set text
 		speaker.text = Global.dialogues[current_key]["speakers"][i]
 		speech.text = Global.dialogues[current_key]["text"][i]
+		# Set images
 		portrait_texture_rect.texture = portraits[Global.dialogues[current_key]["icons"][i]]
+		if Global.dialogues[current_key]["speakers"][i] in good_characters:
+			flower.texture = living_flower
+		else:
+			flower.texture = dead_flower
+		
 		if Input.is_action_just_pressed("enter"):
 			if i < Global.dialogues[current_key]["text"].size() - 1:
 				next_line()
